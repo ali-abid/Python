a43411d7d8608c44e898e8215c6e15be1ffda314
ali-abid <worktimee@gmail.com>
2015-03-10 14:08:57 +0000

    Read and write file

--- readFile.py
+++ readFile.py
@@ -1,6 +1,15 @@
+# Read and Write file.
+# Version 1.0 (Feb, 2015).
+# Abid Ali,
+# IMaR Gateway Technology, Institute of Technology Tralee
+
 import csv
-file = open('C:\Dev\workspace\Python\xyz30round3direction.csv')
+file = open('C:\Dev\workspace\Python/xyz30round3direction.csv')
 reader = csv.reader(file)
+file_output = open("output.txt", "w")
+
 for line in reader:
-	print(line[0],line[1],line[2])
+	print(line[0],line[1],line[2], file = file_output)
+
 file.close
+file_output.close()
